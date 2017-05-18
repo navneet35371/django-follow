30935eef2d2e5dc0e89f89acbafb0c0a5a091479
Ahmed Salman Noor <ahmed.salman.noor@gmail.com>
2017-05-19 03:44:50 +0500

    white label follow

--- follow/models.py
+++ follow/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User, AnonymousUser
 from django.db import models
 from django.db.models.query import QuerySet
@@ -5,6 +6,7 @@ from django.db.models.signals import post_save, post_delete
 from .registry import model_map
 from .signals import followed, unfollowed
 import inspect
+
 
 class FollowManager(models.Manager):
     def fname(self, model_or_obj_or_qs):
@@ -17,7 +19,7 @@ class FollowManager(models.Manager):
             cls = model_or_obj_or_qs if inspect.isclass(model_or_obj_or_qs) else model_or_obj_or_qs.__class__
             _, fname = model_map[cls]
         return fname
-    
+
     def create(self, user, obj, **kwargs):
         """
         Create a new follow link between a user and an object
@@ -28,7 +30,7 @@ class FollowManager(models.Manager):
         follow.target = obj
         follow.save()
         return follow
-            
+
     def get_or_create(self, user, obj, **kwargs):
         """ 
         Almost the same as `FollowManager.objects.create` - behaves the same 
@@ -40,25 +42,32 @@ class FollowManager(models.Manager):
         if not self.is_following(user, obj):
             return self.create(user, obj, **kwargs), True
         return self.get_follows(obj).get(user=user), False
-    
+
     def is_following(self, user, obj):
         """ Returns `True` or `False` """
         if isinstance(user, AnonymousUser):
-            return False        
+            return False
         return 0 < self.get_follows(obj).filter(user=user).count()
 
     def get_follows(self, model_or_obj_or_qs):
         """
         Returns all the Follow objects associated with a certain model, object or queryset..
         """
-        fname = self.fname(model_or_obj_or_qs)
+        from people.models import PeopleWhiteLabel, UserProfile
+        people = None
+        if settings.SITE_ID > 1:
+            white_label_people = PeopleWhiteLabel.objects.filter(site=settings.SITE_ID).values_list('id', flat=True)
+            people = UserProfile.objects.filter(white_label_site__in=white_label_people).values_list('user', flat=True)
 
+        fname = self.fname(model_or_obj_or_qs)
         if isinstance(model_or_obj_or_qs, QuerySet):
             return self.filter(**{'%s__in' % fname: model_or_obj_or_qs})
-        
+
         if inspect.isclass(model_or_obj_or_qs):
             return self.exclude(**{fname:None})
 
+        if people:
+            return self.filter(**{fname:model_or_obj_or_qs}).filter(user__in=people)
         return self.filter(**{fname:model_or_obj_or_qs})
 
     def get_follows_subset(self, model_or_obj_or_qs, sIndex, lIndex):
@@ -66,16 +75,16 @@ class FollowManager(models.Manager):
         Returns all the Follow objects associated with a certain model, object or queryset..
         """
         fname = self.fname(model_or_obj_or_qs)
-        
+
         if isinstance(model_or_obj_or_qs, QuerySet):
             return self.filter(**{'%s__in' % fname: model_or_obj_or_qs})[sIndex:lIndex]
-        
+
         if inspect.isclass(model_or_obj_or_qs):
             return self.exclude(**{fname:None})[sIndex:lIndex]
-            
+
         return self.filter(**{fname:model_or_obj_or_qs})[sIndex:lIndex]
 
-    
+
 class Follow(models.Model):
     """
     This model allows a user to follow any kind of object. The followed
@@ -96,10 +105,10 @@ class Follow(models.Model):
                 if hasattr(self, fname) and getattr(self, fname):
                     return getattr(self, fname)
             except Model.DoesNotExist:
-                # In case the target was deleted in the previous transaction 
+                # In case the target was deleted in the previous transaction
                 # it's already gone from the db and this throws DoesNotExist.
                 return None
-    
+
     def _set_target(self, obj):
         for _, fname in model_map.values():
             setattr(self, fname, None)
@@ -107,29 +116,31 @@ class Follow(models.Model):
             return
         _, fname = model_map[obj.__class__]
         setattr(self, fname, obj)
-        
+
     target = property(fget=_get_target, fset=_set_target)
+
 
 def follow_dispatch(sender, instance, created=False, **kwargs):
     if created:
         followed.send(instance.target.__class__, user=instance.user, target=instance.target, instance=instance)
 
+
 def unfollow_dispatch(sender, instance, **kwargs):
     # FIXME: When deleting out of the admin, django *leaves* the transaction
     # management after the user is deleted and then starts deleting all the
-    # associated objects. This breaks the unfollow signal. Looking up 
+    # associated objects. This breaks the unfollow signal. Looking up
     # `instance.user` will throw a `DoesNotExist` exception.  The offending
     # code is in django/db/models/deletion.py#70
-    # At least that's what the error report looks like and I'm a bit short 
-    # on time to investigate properly. 
+    # At least that's what the error report looks like and I'm a bit short
+    # on time to investigate properly.
     # Unfollow handlers should be aware that both target and user can be `None`
     try:
         user = instance.user
     except User.DoesNotExist:
         user = None
-    
+
     unfollowed.send(instance.target.__class__, user=user, target=instance.target, instance=instance)
-    
-    
+
+
 post_save.connect(follow_dispatch, dispatch_uid='follow.follow_dispatch', sender=Follow)
 post_delete.connect(unfollow_dispatch, dispatch_uid='follow.unfollow_dispatch', sender=Follow)
